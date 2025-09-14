d8f26b4e3f0678ac7bade6ad383726a963231f6e
Suraj Kumar <suraj.20227053@mnnit.ac.in>
2025-09-14 16:04:36 +0530

    stackswap

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
     <div className="navbar bg-base-300">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost text-xl">
-          👩‍💻 DevTinder
+          👩‍💻 Stackswap
         </Link>
       </div>
       {user && (
